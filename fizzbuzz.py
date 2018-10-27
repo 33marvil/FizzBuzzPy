40b4d1ee121db1a3833f7bdd23ac7629f9395e25
33marvil <martinramirezcruz71@gmail.com>
2018-10-27 12:32:06 -0500

    FizzBuzz-in-Pyton-version-3

---
+++ fizzbuzz.py
@@ -0,0 +1,8 @@
+def fizzbuzz(num):
+    if num % 3 == 0:
+        return "Fizz"
+    elif num % 5 == 0:
+        return "Buzz"
+
+for multiple in range(1, 101):
+    print(fizzbuzz(multiple))
--- test_fizzbuzz.py
+++ test_fizzbuzz.py
@@ -1,5 +1,20 @@
 import unittest
-from fizzbuzz import is_fizzbuzz
+
+from fizzbuzz import fizzbuzz
+
+class FizzBuzzTestCase(unittest.TestCase):
+    """Tests for `fizzbuzz.py`."""
+
+    def test_is_multiple_of_3(self):
+        """is multiple of three return Fizz"""
+        self.assertTrue(fizzbuzz(27))
+
+    # def test_is_multiple_of_5(self):
+    #     """is multiple of five return Buzz"""
+    #     self.assertTrue(fizzbuzz(30))
+    #
+    # def test_is_(arg):
+    #     pass
 
 
 
